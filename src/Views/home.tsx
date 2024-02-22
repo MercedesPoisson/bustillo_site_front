551fad4d29b2405b9eeba534b266180c30d11fa7
MercedesPoisson <mercedespoisson@yahoo.com>
2024-02-22 16:50:20 -0300

    layout

--- src/Views/home.tsx
+++ src/Views/home.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+// import { useEffect, useState } from 'react';
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
-    return(
-        <div>
-            <h1>inicio</h1>
+  // const [isLargeScreen, setIsLargeScreen] = useState(true);
+  // useEffect(() => {
+  //     const handleResize = () => {
+  //         setIsLargeScreen(window.innerWidth >= 600);
+  //     };
+
+  //     handleResize();
+  //     window.addEventListener("resize", handleResize);
+
+  //     return () => {
+  //         window.removeEventListener("resize", handleResize);
+  //     };
+  // }, []);
+
+  return (
+    <div className="flex flex-col flex-grow-1 flex-shrink-0">
+      <NavBar />
+      <div className="flex-grow flex">
+        <div className="w-full max-w-3xl mx-4 sm:mx-auto">
+          <Outlet />
         </div>
-    )
-}
+      </div>
+      <h1>inicio</h1>
+    </div>
+  );
+};
 
 export default Home;
